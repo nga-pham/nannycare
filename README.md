2638a7ea2fcaa92b980404ce51cdc986445bd753
ngapham <nga.pham.cs83@gmail.com>
2025-09-27 10:21:34 +0700

    change config

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Reference
 Front end mock up of this project is in here: https://lovable.dev/projects/993a7f43-8391-4a72-816e-fa994863b632
+
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import plugin from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [plugin()],
     server: {
-        port: 52407,
+        port: 8080,
     }
 })

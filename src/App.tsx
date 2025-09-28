7d487fc7719751cc5070e39ac32245acf55a9981
ngapham <nga.pham.cs83@gmail.com>
2025-09-28 12:18:41 +0700

    change index page

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import { Toaster } from "../src/components/ui/toaster";
+import { Toaster as Sonner } from "../src/components/ui/sooner";
+import { TooltipProvider } from "../src/components/ui/tooltip";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "../src/pages/NotFound";
+
+
+const App = () => (
+    <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Index />} />
+                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
+    </TooltipProvider>
+);
+
+export default App
---
+++ src/pages/Index.tsx
@@ -0,0 +1,7 @@
+import IeltsLanding from "../components/landingPage";
+
+const Index = () => {
+    return <IeltsLanding />;
+};
+
+export default Index;

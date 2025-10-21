a31e07adcf8c781e1cd47b5ac0b663aaed877d56
ngapham <nga.pham.cs83@gmail.com>
2025-10-21 15:43:26 +0700

    filter recommended nannies

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -20,7 +20,7 @@ const ServiceCard = ({ photos, name, rating, numberOfRating, address, experience
                       <Card.Body>
                         <Card.Title><p style={{color: '#000', textAlign: 'left'}}>{name}</p></Card.Title>
                         <Card.Text style={{color: '#000', textAlign: 'left', fontSize: '1rem'}}>
-                        <p style={{fontWeight: 'bold'}}>{rating} <Star fill="orange" strokeWidth={1} size={12} style={{marginBottom: '0.25rem'}} /> ({numberOfRating})</p>
+                        <p style={{fontWeight: 'bold'}}>{rating} <Star fill="orange" strokeWidth={0} size={12} style={{marginBottom: '0.25rem'}} /> ({numberOfRating})</p>
                         <p style={{color: 'rgba(0, 0, 0, 0.5)'}}>{address}</p>
                         <p>{experience} years experience</p>
                         <p>{expertise.map(skill => (
@@ -57,26 +57,24 @@ const ServicesSection = () => {
                   className="h-100 justify-content-center align-items-center"
                   gap={3}
                 >
-                  {block.map((nanny, idx) => (
-                    <Card bg="light" style={{ width: "20rem" }}>
-                      <Card.Img variant="top" src={nanny.photos} />
-                      <Card.Body>
-                        <Card.Title><p style={{color: '#000', textAlign: 'left'}}>{nanny.name}</p></Card.Title>
-                        <Card.Text style={{color: '#000', textAlign: 'left', fontSize: '1rem'}}>
-                        <p style={{fontWeight: 'bold'}}>{nanny.rating} <Star fill="orange" strokeWidth={1} size={12} style={{marginBottom: '0.25rem'}} /> ({nanny.numberOfRating})</p>
-                        <p style={{color: 'rgba(0, 0, 0, 0.5)'}}>{nanny.address}</p>
-                        <p>{nanny.experience} years experience</p>
-                        <p>{nanny.expertise.map(skill => (
-                            <span key={skill} style={{marginRight: '0.5rem', padding: '0.25rem 0.5rem', border: '1px solid #ccc', borderRadius: '0.25rem', fontSize: '0.875rem'}}>{skill}</span>
-                        ))}</p>
-                        </Card.Text>
-                      </Card.Body>
-                    </Card>
-                  ))}
+                                    {block.map((nanny, idx) => {
+                                        if (nanny.recommended) {
+                                            return (
+                                                <ServiceCard
+                                                    key={idx}
+                                                    photos={nanny.photos}
+                                                    name={nanny.name}
+                                                    rating={nanny.rating}
+                                                    numberOfRating={nanny.numberOfRating}
+                                                    address={nanny.address}
+                                                    experience={nanny.experience}
+                                                    expertise={nanny.expertise} />
+                                            )
+                                        }
+                                    })}
                 </Stack>
               </Carousel.Item>
                         )) }
-                        
                     </Carousel>
                 </Row>
                 <Row className="mb-4">
